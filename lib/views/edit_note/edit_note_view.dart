37c0cfd90fcff34cfa0e49e58959093097775208
peeteerr <peter.fawzii@gmail.com>
2023-01-16 17:32:58 +0200

    add initial implementation of view

--- lib/views/edit_note/edit_note_view.dart
+++ lib/views/edit_note/edit_note_view.dart
@@ -6,12 +6,44 @@ class EditNoteView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-      ),
-      body: Container(
-        child: Text('Edit Here'),
-      ),
-    );
+        appBar: AppBar(
+          elevation: 0,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.check),
+            ),
+          ],
+        ),
+        body: SizedBox(
+          height: 400,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              (Form(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        keyboardType: TextInputType.text,
+                        maxLines: 1,
+                        decoration: const InputDecoration(hintText: 'Enter Title'),
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 4,
+                        decoration: const InputDecoration(hintText: 'Enter details'),
+                      ),
+                    ],
+                  ),
+                ),
+              )),
+            ],
+          ),
+        ));
   }
 }
